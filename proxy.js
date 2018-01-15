4814bac8a59a1511ee5c88f10d8a6cb77097cc63
ivanwonder <ivanwonder@outlook.com>
2018-01-15 14:41:41 +0800

    +添加代理服务器

---
+++ proxy.js
@@ -0,0 +1,11 @@
+var MiniProxy = require('./MiniProxy')
+
+var myProxy = new MiniProxy({
+  'port': 9393,
+  'onBeforeRequest': function (requestOptions) {
+    console.log('proxy request :' + (requestOptions.path || ''))
+  }
+})
+
+myProxy.start()
+console.log('proxy start at 9393')
